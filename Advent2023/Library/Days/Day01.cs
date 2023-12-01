b61c26b8f4011b5b0f79eea8b5c0de294a9cf6b4
Michael Bond <michael@bondcodes.com>
2023-12-01 00:43:36 -0500

    Add problem 1 solution 1

---
+++ Advent2023/Library/Days/Day01.cs
@@ -0,0 +1,57 @@
+﻿
+namespace Advent2023.Library;
+
+public class Day01 : BaseLibraryDay
+{
+    private string _input;
+
+    public Day01()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        List<int> numbers = [];
+        foreach (var line in _input.Split("\n"))
+        {
+            if (line is not { Length: > 0 })
+            {
+                continue;
+            }
+
+            var numberString = string.Empty;
+
+            for (var i = 0; i < line.Length; i++)
+            {
+                var c = line[i];
+                if (char.IsNumber(c))
+                {
+                    numberString += c;
+                    break;
+                }
+            }
+
+            for (var i = line.Length - 1; i >= 0; i--)
+            {
+                var c = line[i];
+                if (char.IsNumber(c))
+                {
+                    numberString += c;
+                    break;
+                }
+            }
+
+            if (int.TryParse(numberString, out var number))
+            {
+                numbers.Add(number);
+            }
+        }
+        return new(numbers.Sum().ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new("");
+    }
+}
