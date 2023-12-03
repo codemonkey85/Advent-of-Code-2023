cb864831710c6a8c4a16fcc521a0a7f758b31337
Michael <michael@bondcodes.com>
2023-12-03 15:19:06 -0500

    Update DayTemplate.cs

--- Advent2023/Library/Days/DayTemplate.cs
+++ Advent2023/Library/Days/DayTemplate.cs
@@ -1,13 +1,13 @@
 ﻿namespace Advent2023.Library.Days;
 
-public class DayTemplate : BaseLibraryDay
-{
-    private readonly string _input;
+//public class DayTemplate : BaseLibraryDay
+//{
+//    private readonly string _input;
 
-    public DayTemplate() =>
-        _input = File.ReadAllText(InputFilePath);
+//    public DayTemplate() =>
+//        _input = File.ReadAllText(InputFilePath);
 
-    public override ValueTask<string> Solve_1() => new();
+//    public override ValueTask<string> Solve_1() => new();
 
-    public override ValueTask<string> Solve_2() => new();
-}
+//    public override ValueTask<string> Solve_2() => new();
+//}
