3295f3cac2983c9d78113b072654cf55373568f0
Michael Bond <michael@bondcodes.com>
2023-12-08 00:13:24 -0500

    Remove web app

--- Advent2023/WebUi/GlobalUsings.cs
+++
@@ -1,7 +0,0 @@
-﻿global using Advent2023.Library;
-global using Advent2023.WebUi;
-global using AoCHelper;
-global using Microsoft.AspNetCore.Components.Web;
-global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-global using MudBlazor;
-global using MudBlazor.Services;
