f7c67dd4f9aefe7530a15d35c334a95001b5b6f3
Michael Bond <michael@bondcodes.com>
2023-12-10 00:39:00 -0500

    Stub out day 10

---
+++ Advent2023/Library/Days/Day10.cs
@@ -0,0 +1,14 @@
+﻿namespace Advent2023.Library.Days;
+
+// https://adventofcode.com/2023/day/10
+public class Day10 : BaseLibraryDay
+{
+    private readonly string _input;
+
+    public Day10() =>
+        _input = File.ReadAllText(InputFilePath);
+
+    public override ValueTask<string> Solve_1() => new();
+
+    public override ValueTask<string> Solve_2() => new();
+}
