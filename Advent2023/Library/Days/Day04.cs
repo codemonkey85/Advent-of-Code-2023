f3e09d9a5f6a8ed37d8a5be99c2bbb029b4761b4
Michael <michael@bondcodes.com>
2023-12-10 16:52:01 -0500

    Day 4 part 1 done, part 2 started

--- Advent2023/Library/Days/Day04.cs
+++ Advent2023/Library/Days/Day04.cs
@@ -1,4 +1,6 @@
-﻿namespace Advent2023.Library.Days;
+﻿using System.Collections.Generic;
+
+namespace Advent2023.Library.Days;
 
 // https://adventofcode.com/2023/day/4
 public class Day04 : BaseLibraryDay
@@ -17,7 +19,117 @@ public class Day04 : BaseLibraryDay
     public Day04() =>
         _input = File.ReadAllText(InputFilePath);
 
-    public override ValueTask<string> Solve_1() => new();
+    private Dictionary<int, Card> GetCards(string input)
+    {
+        Dictionary<int, Card> cards = [];
+        var localInput = input.Replace("\n\r", "\n");
+        var lines = localInput.Split('\n') ?? [];
+        foreach (var line in lines.Select(l => l.Trim()).Where(l => !string.IsNullOrEmpty(l)))
+        {
+            var cardLineSplit = line.Split(':').Select(l => l.Trim()).ToList() ?? [];
+            var cardNumString = cardLineSplit[0].Replace("Card", string.Empty, StringComparison.OrdinalIgnoreCase).Trim();
 
-    public override ValueTask<string> Solve_2() => new();
+            if (!int.TryParse(cardNumString, out var cardNum))
+            {
+                throw new Exception("Can't parse card number");
+            }
+
+            var cardNumberStrings = cardLineSplit[1].Split('|').Select(c => c.Trim()).ToList() ?? [];
+            var winningNumbers = ConvertNumStringsToNumList(cardNumberStrings[0]) ?? [];
+            var myNumbers = ConvertNumStringsToNumList(cardNumberStrings[1]) ?? [];
+
+            cards.Add(cardNum, new Card
+            {
+                CardNum = cardNum,
+                WinningNumbers = winningNumbers,
+                MyNumbers = myNumbers,
+            });
+        }
+
+        return cards;
+
+        static List<int> ConvertNumStringsToNumList(string numStrings) => numStrings
+                .Split(' ')
+                .Select(c => c.Trim())
+                .Where(c => !string.IsNullOrEmpty(c) && !string.IsNullOrWhiteSpace(c))
+                .Select(c => !int.TryParse(c, out var num) ? throw new Exception("Invalid Number") : num)
+                .Order()
+                .ToList();
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var cards = GetCards(_input)
+            .Select(kvp => kvp.Value);
+
+        return new(cards.Sum(c => c.TotalPoints).ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var cards = GetCards(testInput);
+
+        List<Card> allCards = [];
+        foreach (var (key, card) in cards)
+        {
+            allCards.AddRange(EvaluateCard(cards, key));
+        }
+
+        return new(allCards.Count.ToString());
+
+        static List<Card> EvaluateCard(Dictionary<int, Card> cardsDictionary, int key)
+        {
+            var card = cardsDictionary[key];
+            List<Card> returnedCards = [card];
+            var totalMatchingNumbers = card.TotalMatchingNumbers;
+
+            for (var i = 0; i < totalMatchingNumbers; i++)
+            {
+                //returnedCards.AddRange(EvaluateCard(cardsDictionary, key + i));
+                if (!cardsDictionary.TryGetValue(key + i, out var result))
+                {
+                    throw new Exception("Can't find card");
+                }
+
+                returnedCards.Add(result);
+            }
+
+            return returnedCards;
+        }
+    }
+
+    private class Card()
+    {
+        public int CardNum { get; set; }
+
+        public List<int> WinningNumbers { get; set; } = [];
+
+        public List<int> MyNumbers { get; set; } = [];
+
+        public int TotalPoints
+        {
+            get
+            {
+                if (TotalMatchingNumbers == 0)
+                {
+                    return 0;
+                }
+
+                var totalPoints = 1;
+
+                for (var i = 0; i < TotalMatchingNumbers - 1; i++)
+                {
+                    totalPoints *= 2;
+                }
+
+                return totalPoints;
+            }
+        }
+
+        public int TotalMatchingNumbers =>
+            WinningNumbers.Count == 0 || MyNumbers.Count == 0
+                    ? 0
+                    : MyNumbers
+            .Count(n => WinningNumbers.Contains(n));
+    }
 }
