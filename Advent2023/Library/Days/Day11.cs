0cf04d37edaf6a0180ca804be5e1d4edc6e232a2
Michael Bond <michael@bondcodes.com>
2023-12-12 14:58:04 -0500

    Stub days 11 and 12

---
+++ Advent2023/Library/Days/Day11.cs
@@ -0,0 +1,27 @@
+﻿namespace Advent2023.Library.Days;
+
+// https://adventofcode.com/2023/day/11
+public class Day11 : BaseLibraryDay
+{
+    private readonly string _input;
+
+    private const string testInput = """
+        ...#......
+        .......#..
+        #.........
+        ..........
+        ......#...
+        .#........
+        .........#
+        ..........
+        .......#..
+        #...#.....
+        """;
+
+    public Day11() =>
+        _input = File.ReadAllText(InputFilePath);
+
+    public override ValueTask<string> Solve_1() => new();
+
+    public override ValueTask<string> Solve_2() => new();
+}
---
+++ Advent2023/Library/Days/Day12.cs
@@ -0,0 +1,23 @@
+﻿namespace Advent2023.Library.Days;
+
+// https://adventofcode.com/2023/day/12
+public class Day12 : BaseLibraryDay
+{
+    private readonly string _input;
+
+    private const string testInput = """
+        #.#.### 1,1,3
+        .#...#....###. 1,1,3
+        .#.###.#.###### 1,3,1,6
+        ####.#...#... 4,1,1
+        #....######..#####. 1,6,5
+        .###.##....# 3,2,1
+        """;
+
+    public Day12() =>
+        _input = File.ReadAllText(InputFilePath);
+
+    public override ValueTask<string> Solve_1() => new();
+
+    public override ValueTask<string> Solve_2() => new();
+}
