cccbeb114a8db848e5e312ec258424281ba9c35d
Michael Bond <michael@bondcodes.com>
2023-12-13 09:34:23 -0500

    Update Program.cs

--- Advent2023/Console/Program.cs
+++ Advent2023/Console/Program.cs
@@ -1,2 +1,1 @@
-﻿using Advent2023.Library.Days;
-await Solver.Solve<Day06>(options => options.ProblemAssemblies = [typeof(BaseLibraryDay).Assembly]); // .SolveAll(options => options.ProblemAssemblies = [typeof(BaseLibraryDay).Assembly]);
+﻿await Solver.SolveAll(options => options.ProblemAssemblies = [typeof(BaseLibraryDay).Assembly]);
