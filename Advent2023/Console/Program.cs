6a5e3e9efca70cb4cd0ce8c6c4ac78c19fdf8091
Michael Bond <michael@bondcodes.com>
2023-12-13 09:33:47 -0500

    Stub out day 13

--- Advent2023/Console/Program.cs
+++ Advent2023/Console/Program.cs
@@ -1,1 +1,2 @@
-﻿await Solver.SolveAll(options => options.ProblemAssemblies = [typeof(BaseLibraryDay).Assembly]);
+﻿using Advent2023.Library.Days;
+await Solver.Solve<Day06>(options => options.ProblemAssemblies = [typeof(BaseLibraryDay).Assembly]); // .SolveAll(options => options.ProblemAssemblies = [typeof(BaseLibraryDay).Assembly]);
---
+++ Advent2023/Library/Days/Day13.cs
@@ -0,0 +1,38 @@
+﻿namespace Advent2023.Library.Days;
+
+// https://adventofcode.com/2023/day/13
+public class Day13 : BaseLibraryDay
+{
+    private readonly string _input;
+
+    private const string testInput = """
+        #.##..##.
+        ..#.##.#.
+        ##......#
+        ##......#
+        ..#.##.#.
+        ..##..##.
+        #.#.##.#.
+
+        #...##..#
+        #....#..#
+        ..##..###
+        #####.##.
+        #####.##.
+        ..##..###
+        #....#..#
+        """;
+
+    public Day13() =>
+        _input = File.ReadAllText(InputFilePath);
+
+    public override ValueTask<string> Solve_1()
+    {
+        return new();
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new();
+    }
+}
