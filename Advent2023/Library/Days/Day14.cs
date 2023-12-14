ae9ca2a7fc6cc6a57f29ec22fe93af5da03d377f
Michael Bond <michael@bondcodes.com>
2023-12-14 18:12:10 -0500

    Stub out day 14

---
+++ Advent2023/Library/Days/Day14.cs
@@ -0,0 +1,33 @@
+﻿namespace Advent2023.Library.Days;
+
+// https://adventofcode.com/2023/day/14
+public class Day14 : BaseLibraryDay
+{
+    private readonly string _input;
+
+    private const string testInput = """
+        O....#....
+        O.OO#....#
+        .....##...
+        OO.#O....O
+        .O.....O#.
+        O.#..O.#.#
+        ..O..#O..O
+        .......O..
+        #....###..
+        #OO..#....
+        """;
+
+    public Day14() =>
+        _input = File.ReadAllText(InputFilePath);
+
+    public override ValueTask<string> Solve_1()
+    {
+        return new();
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new();
+    }
+}
