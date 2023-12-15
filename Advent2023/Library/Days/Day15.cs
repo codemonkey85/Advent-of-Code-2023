8eaed6a6c363f36f68664d263caad139d9fba13c
Michael Bond <michael@bondcodes.com>
2023-12-15 09:29:19 -0500

    Stub out day 15

---
+++ Advent2023/Library/Days/Day15.cs
@@ -0,0 +1,26 @@
+﻿namespace Advent2023.Library.Days;
+
+// https://adventofcode.com/2023/day/15
+// ReSharper disable once UnusedType.Global
+public sealed class Day15 : BaseLibraryDay
+{
+    private readonly string input;
+
+    // ReSharper disable once UnusedMember.Local
+    private const string TestInput = """
+                                     HASH
+                                     """;
+
+    public Day15() =>
+        input = File.ReadAllText(InputFilePath);
+
+    public override ValueTask<string> Solve_1()
+    {
+        return new();
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new();
+    }
+}
