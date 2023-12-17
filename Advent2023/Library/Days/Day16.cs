4e69a1d116bf0c8756f331257f98c56c6eaead7b
Michael <michael@bondcodes.com>
2023-12-17 11:23:12 -0500

    Stub out days 16 and 17

---
+++ Advent2023/Library/Days/Day16.cs
@@ -0,0 +1,35 @@
+﻿namespace Advent2023.Library.Days;
+
+// https://adventofcode.com/2023/day/16
+// ReSharper disable once UnusedType.Global
+public sealed class Day16 : BaseLibraryDay
+{
+    private readonly string input;
+
+    // ReSharper disable once UnusedMember.Local
+    private const string TestInput = """
+                                     .|...\....
+                                     |.-.\.....
+                                     .....|-...
+                                     ........|.
+                                     ..........
+                                     .........\
+                                     ..../.\\..
+                                     .-.-/..|..
+                                     .|....-|.\
+                                     ..//.|....
+                                     """;
+
+    public Day16() =>
+        input = File.ReadAllText(InputFilePath);
+
+    public override ValueTask<string> Solve_1()
+    {
+        return new();
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new();
+    }
+}
---
+++ Advent2023/Library/Days/Day17.cs
@@ -0,0 +1,38 @@
+﻿namespace Advent2023.Library.Days;
+
+// https://adventofcode.com/2023/day/17
+// ReSharper disable once UnusedType.Global
+public sealed class Day17 : BaseLibraryDay
+{
+    private readonly string input;
+
+    // ReSharper disable once UnusedMember.Local
+    private const string TestInput = """
+                                     2413432311323
+                                     3215453535623
+                                     3255245654254
+                                     3446585845452
+                                     4546657867536
+                                     1438598798454
+                                     4457876987766
+                                     3637877979653
+                                     4654967986887
+                                     4564679986453
+                                     1224686865563
+                                     2546548887735
+                                     4322674655533
+                                     """;
+
+    public Day17() =>
+        input = File.ReadAllText(InputFilePath);
+
+    public override ValueTask<string> Solve_1()
+    {
+        return new();
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new();
+    }
+}
