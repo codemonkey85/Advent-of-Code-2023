59b77afc0d5dbfb7fe6fb8adb213a228f48d489d
Michael Bond <codemonkey8510@icloud.com>
2023-12-19 15:26:38 -0500

    Stub out days 18 and 19

---
+++ Advent2023/Library/Days/Day19.cs
@@ -0,0 +1,42 @@
+﻿namespace Advent2023.Library.Days;
+
+// https://adventofcode.com/2023/day/19
+// ReSharper disable once UnusedType.Global
+public sealed class Day19 : BaseLibraryDay
+{
+    private readonly string input;
+
+    // ReSharper disable once UnusedMember.Local
+    private const string TestInput = """
+                                     px{a<2006:qkq,m>2090:A,rfg}
+                                     pv{a>1716:R,A}
+                                     lnx{m>1548:A,A}
+                                     rfg{s<537:gd,x>2440:R,A}
+                                     qs{s>3448:A,lnx}
+                                     qkq{x<1416:A,crn}
+                                     crn{x>2662:A,R}
+                                     in{s<1351:px,qqz}
+                                     qqz{s>2770:qs,m<1801:hdj,R}
+                                     gd{a>3333:R,R}
+                                     hdj{m>838:A,pv}
+
+                                     {x=787,m=2655,a=1222,s=2876}
+                                     {x=1679,m=44,a=2067,s=496}
+                                     {x=2036,m=264,a=79,s=2244}
+                                     {x=2461,m=1339,a=466,s=291}
+                                     {x=2127,m=1623,a=2188,s=1013}
+                                     """;
+
+    public Day19() =>
+        input = File.ReadAllText(InputFilePath);
+
+    public override ValueTask<string> Solve_1()
+    {
+        return new();
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new();
+    }
+}
