b621ae631b1932fd47b213746f23e3f02e203241
Michael Bond <michael@bondcodes.com>
2023-12-21 22:33:03 -0500

    Stub out days 20 and 21

---
+++ Advent2023/Library/Days/Day20.cs
@@ -0,0 +1,30 @@
+﻿namespace Advent2023.Library.Days;
+
+// https://adventofcode.com/2023/day/20
+// ReSharper disable once UnusedType.Global
+public sealed class Day20 : BaseLibraryDay
+{
+    private readonly string input;
+
+    // ReSharper disable once UnusedMember.Local
+    private const string TestInput = """
+                                     broadcaster -> a, b, c
+                                     %a -> b
+                                     %b -> c
+                                     %c -> inv
+                                     &inv -> a
+                                     """;
+
+    public Day20() =>
+        input = File.ReadAllText(InputFilePath);
+
+    public override ValueTask<string> Solve_1()
+    {
+        return new();
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new();
+    }
+}
---
+++ Advent2023/Library/Days/Day21.cs
@@ -0,0 +1,36 @@
+﻿namespace Advent2023.Library.Days;
+
+// https://adventofcode.com/2023/day/21
+// ReSharper disable once UnusedType.Global
+public sealed class Day21 : BaseLibraryDay
+{
+    private readonly string input;
+
+    // ReSharper disable once UnusedMember.Local
+    private const string TestInput = """
+                                     ...........
+                                     .....###.#.
+                                     .###.##..#.
+                                     ..#.#...#..
+                                     ....#.#....
+                                     .##..S####.
+                                     .##..#...#.
+                                     .......##..
+                                     .##.#.####.
+                                     .##..##.##.
+                                     ...........
+                                     """;
+
+    public Day21() =>
+        input = File.ReadAllText(InputFilePath);
+
+    public override ValueTask<string> Solve_1()
+    {
+        return new();
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new();
+    }
+}
