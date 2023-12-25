fd3a89cba80c4fca1469ca3bde82e40ed263f88a
Michael Bond <michael@bondcodes.com>
2023-12-25 12:56:37 -0500

    Stub out remaining days

--- Advent2023/Library/Days/Day24.cs
+++ Advent2023/Library/Days/Day24.cs
@@ -8,7 +8,11 @@ public sealed class Day24 : BaseLibraryDay
 
     // ReSharper disable once UnusedMember.Local
     private const string TestInput = """
-                                     
+                                     19, 13, 30 @ -2,  1, -2
+                                     18, 19, 22 @ -1, -1, -2
+                                     20, 25, 34 @ -2, -2, -4
+                                     12, 31, 28 @ -1, -2, -1
+                                     20, 19, 15 @  1, -5, -3
                                      """;
 
     public Day24() =>
--- Advent2023/Library/Days/Day25.cs
+++ Advent2023/Library/Days/Day25.cs
@@ -8,7 +8,19 @@ public sealed class Day25 : BaseLibraryDay
 
     // ReSharper disable once UnusedMember.Local
     private const string TestInput = """
-                                     
+                                     jqt: rhn xhk nvd
+                                     rsh: frs pzl lsr
+                                     xhk: hfx
+                                     cmg: qnr nvd lhk bvb
+                                     rhn: xhk bvb hfx
+                                     bvb: xhk hfx
+                                     pzl: lsr hfx nvd
+                                     qnr: nvd
+                                     ntq: jqt hfx bvb xhk
+                                     nvd: lhk
+                                     lsr: lhk
+                                     rzs: qnr cmg lsr rsh
+                                     frs: qnr lhk lsr
                                      """;
 
     public Day25() =>
